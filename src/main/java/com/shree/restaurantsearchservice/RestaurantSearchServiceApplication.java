d1d9ed931c7b087e6aae229772c045a61d7f7f76
Shree <reachoutshree@gmail.com>
2024-06-01 18:10:44 +0530

    intial commit

---
+++ src/main/java/com/shree/restaurantsearchservice/RestaurantSearchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.shree.restaurantsearchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantSearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantSearchServiceApplication.class, args);
+	}
+
+}
