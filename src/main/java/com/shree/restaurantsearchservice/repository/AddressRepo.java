610c87ce84b3a5a63ec6d7c17915055812a40351
Shree <reachoutshree@gmail.com>
2024-06-01 19:23:17 +0530

    #1 Repository class added

---
+++ src/main/java/com/shree/restaurantsearchservice/repository/AddressRepo.java
@@ -0,0 +1,9 @@
+package com.shree.restaurantsearchservice.repository;
+
+import com.shree.restaurantsearchservice.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AddressRepo extends JpaRepository<Address, Long> {
+}
---
+++ src/main/java/com/shree/restaurantsearchservice/repository/RestaurantRepo.java
@@ -0,0 +1,11 @@
+package com.shree.restaurantsearchservice.repository;
+
+import com.shree.restaurantsearchservice.model.Restaurant;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RestaurantRepo extends JpaRepository<Restaurant, Long>, Specification<Restaurant> {
+
+}
